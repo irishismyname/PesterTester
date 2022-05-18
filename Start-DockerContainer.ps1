7b88cd7976f2227fbc45301f872225afa7375247
irishismyname <34629432+irishismyname@users.noreply.github.com>
2022-05-17 22:31:25 -0400

    Use $pwd

--- Start-DockerContainer.ps1
+++ Start-DockerContainer.ps1
@@ -1,8 +1,6 @@
-$CurrentPath = (Resolve-Path .).Path
-
 docker run -it `
   --user ContainerAdministrator `
-  -v "${CurrentPath}:C:\source" `
+  -v "${pwd}:C:\source" `
   mcr.microsoft.com/powershell:nanoserver-20h2 `
   pwsh -Command "
   Install-Module Pester -Force -Scope AllUsers -SkipPublisherCheck; `
@@ -10,5 +8,6 @@ docker run -it `
   Set-Location ./source;
   Invoke-Pester;
   Write-Warning '-------- Running with the -OutputFile set will fail --------';
-  Invoke-Pester -OutputFile testResults.xml;
+  `$Result = Invoke-Pester -OutputFile testResults.xml;
+  $Result;
   "
